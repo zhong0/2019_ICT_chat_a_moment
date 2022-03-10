4db99505844ee524bab651fbb6ef89563260ca4a
zhong0 <zhong0413@gmail.com>
2022-03-10 20:00:06 +0800

    Add files via upload

---
+++ face-detection/faceEmotionDocker/filetest.py
@@ -0,0 +1,2 @@
+with open('faceEmotion.txt','r') as infile:
+	print(infile.read().split('\n')[1])
---
+++ face-detection/faceEmotionDocker/trytest.py
@@ -0,0 +1,2 @@
+with open('/notebooks/faceEmoOnD/faceEmotionTrigger.txt','w') as opfile:
+	opfile.write("323\nhi")
